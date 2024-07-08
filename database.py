bd5df0ecc2332492169a1d711b57ed3b3a4a09a1
YUTIKS <yutiks15@gmail.com>
2024-07-08 17:18:38 +0100

    add saving clothes to the db

--- database.py
+++ database.py
@@ -1,38 +1,39 @@
+import json
+
+
 class Database:
     def __init__(self, name, keys: list[str]):
-        self.name = name
+        self.name = f"{name}.json"
         self.keys = keys
         try:
-            self.old_file()
-            with open(self.name, "r"):
-                pass
+            with open(self.name, "r") as json_file:
+                self.dict = json.load(json_file)
         except FileNotFoundError:
             self.new_file()
-
-        self.dict = self.make_dict()
+            self.dict = {}
 
     def check(self, pk):
         return pk in self.dict
 
     def new_file(self):
-        main = open(self.name, "w")
-        for key in self.keys:
-            main.write(f"{key}/")
-        main.write("\n")
-        main.close()
+        with open(self.name, "w") as json_file:
+            json.dump({}, json_file)
 
-    def old_file(self):
-        pass
-
-    def create(self, inf):
-        if not self.check(inf[0]):
-            self.dict[inf[0]] = dict(self.connect_keys(inf))
+    def create(self, record):
+        pk = record[0]
+        data = dict(zip(self.keys[1:], record[1:]))
+        if not self.check(pk):
+            self.dict[pk] = data
+            self.save()
         else:
             raise ValueError("Record exists")
 
-    def update(self, inf):
-        if self.check(inf[0]):
-            self.dict[inf[0]] = dict(self.connect_keys(inf))
+    def update(self, record):
+        pk = record[0]
+        data = dict(zip(self.keys[1:], record[1:]))
+        if self.check(pk):
+            self.dict[pk] = data
+            self.save()
         else:
             raise ValueError("Record not found")
 
@@ -47,39 +48,19 @@ class Database:
             connected_list.append((self.keys[i], values[i]))
         return connected_list
 
-    def make_dict(self):
-        the_dict = {}
-        main = open(self.name, "r")
-        lines = main.readlines()
-        main.close()
-        for i in lines[1:]:
-            value = i.split("/")
-            the_dict[value[0]] = dict(self.connect_keys(value))
-        return the_dict
-
     def save(self):
-        with open(self.name, "r") as main:
-            lines = main.readlines()
-        with open(self.name, "w") as main:
-            main.write(lines[0])
-            for j in self.dict:
-                main.write(f"{j}/")
-                for i in range(1, len(self.keys)):
-                    main.write(f"{self.dict[j][self.keys[i]]}/")
-                main.write("\n")
+        with open(self.name, "w") as json_file:
+            json.dump(self.dict, json_file, indent=3)
 
 
-# cars_db = Database("cars.text", ["number-plate", "car-make", "model", "release-date"])
-# cars_db.create(["kz-374vaz-01", "BMW", "M5 Competition", "2017"])
-# cars_db.create(["kz-001AAA-02", "Mercedes", "GT 63 S", "2018"])
-# cars_db.update(["kz-577ASA-14", "Range Rover", "Sport L494", "2022"])
-# print(cars_db.dict)
-# print(cars_db.read("kz-577ASA-14"))
-# cars_db.save()
-
-player_db = Database("player.text", ["PlayerID", "satiety", "money", "happiness", "health"])
-# player_db.create(["User1", 100, 10000, 100, 100])
-# player_db.update(["User1", 100, 10000, 100, 100])
-# player_db.save()
-# print(player_db.read("User1"))
+# player_db = Database("player", ["PlayerID", "indicators", "clothes"])
+# player_db.update(["User1", {"satiety": 100, "money": 10000,
+#                                                  "happiness": 100, "health": 100},
+#                                        {"wear_num": []}])
+# # player_db.save()
+# # print(player_db.read("User1"))
+#
+# read_data = player_db.read("User1")
+# satiety_value = read_data["indicators"]["satiety"]
+# print(satiety_value)
 # print(player_db.make_dict())
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import button as butt
-from settings import *
-import pet
-import indicator
-import mini_game
 import random
-import database
+
+from mini_game import FallingToys
+from pet import Pet
+from button import Button
+from database import Database
+from indicator import Indicator
+from settings import *
+
 pg.init()
 
 
@@ -16,45 +18,37 @@ class Game:
         self.current_tub = 0
         self.clock = pg.time.Clock()
         # BUTTON DATA -->
-        self.button_food = butt.Button("food", 1235, 100)
-        self.button_clothe = butt.Button("clothes", 1235, 200)
-        self.button_game = butt.Button("mini game", 1235, 300)
-        self.button_upgrade = butt.Button("upgrade", 1235, 400)
+        self.button_food = Button("food", 1235, 100)
+        self.button_clothe = Button("clothes", 1235, 200)
+        self.button_game = Button("mini game", 1235, 300)
+        self.button_upgrade = Button("upgrade", 1235, 400)
         self.main_buttons = [self.button_food, self.button_clothe, self.button_game, self.button_upgrade]
-        self.button_buy = butt.Button("buy", 618, 600)
-        self.button_eat = butt.Button("eat", 618, 600)
-        self.button_wear = butt.Button("wear", 190, 570)
+        self.button_buy = Button("buy", 618, 600)
+        self.button_eat = Button("eat", 618, 600)
+        self.button_wear = Button("wear", 190, 570)
         self.buttons = [self.button_buy, self.button_eat, self.button_wear]
         # PET DATA -->
-        self.pet_main = pet.Pet(530, 200, PET)
-        self.pet_game = pet.Pet(650, 550, PET_MINI)
+        self.pet_main = Pet(530, 200, PET)
+        self.pet_mini = Pet(650, 550, PET_MINI)
         # INDICATOR DATA -->
-        self.happiness = indicator.Indicator(HAPPINESS, 5, 5)
-        self.satiety = indicator.Indicator(SATIETY, 5, 145)
-        self.health = indicator.Indicator(HEALTH, 5, 285)
-        self.money = indicator.Indicator(MONEY, 5, 425)
+        self.happiness = Indicator(HAPPINESS, 5, 5)
+        self.satiety = Indicator(SATIETY, 5, 145)
+        self.health = Indicator(HEALTH, 5, 285)
+        self.money = Indicator(MONEY, 5, 425)
         self.indicators = [self.happiness, self.satiety, self.health, self.money]
         # MENU INDICATORS -->
-        self.bought_top_label_on = indicator.Indicator(TOP_LABEL_ON, 100, 0, "bought")
-        self.worn_top_label_on = indicator.Indicator(TOP_LABEL_ON, 100, 100, "worn")
-        self.bought_top_label_off = indicator.Indicator(TOP_LABEL_OFF, 100, 0, "didn't buy")
-        self.worn_top_label_off = indicator.Indicator(TOP_LABEL_OFF, 100, 100, "taken off")
+        self.bought_top_label_on = Indicator(TOP_LABEL_ON, 100, 0, "bought")
+        self.worn_top_label_on = Indicator(TOP_LABEL_ON, 100, 100, "worn")
+        self.bought_top_label_off = Indicator(TOP_LABEL_OFF, 100, 0, "didn't buy")
+        self.worn_top_label_off = Indicator(TOP_LABEL_OFF, 100, 100, "taken off")
         # FOOD -->
-        self.apple = indicator.Indicator(APPLE, 640, 360, ">>Apple<<")
-        self.bone = indicator.Indicator(BONE, 640, 360, ">>Bone<<")
-        self.dog_food = indicator.Indicator(DOG_FOOD, 640, 360, ">>Dog Food<<")
-        self.dog_food_elite = indicator.Indicator(DOG_FOOD_ELITE, 630, 360, ">>Dog Food Elite<<")
-        self.meat = indicator.Indicator(MEAT, 640, 360, ">>Meat<<")
-        self.medicine = indicator.Indicator(MEDICINE, 640, 360, ">>Medicine<<")
+        self.apple = Indicator(APPLE, 640, 360, ">>Apple<<")
+        self.bone = Indicator(BONE, 640, 360, ">>Bone<<")
+        self.dog_food = Indicator(DOG_FOOD, 640, 360, ">>Dog Food<<")
+        self.dog_food_elite = Indicator(DOG_FOOD_ELITE, 630, 360, ">>Dog Food Elite<<")
+        self.meat = Indicator(MEAT, 640, 360, ">>Meat<<")
+        self.medicine = Indicator(MEDICINE, 640, 360, ">>Medicine<<")
         self.food = [self.apple, self.bone, self.dog_food, self.dog_food_elite, self.meat, self.medicine]
-        # CLOTHES & ITEMS -->
-        self.glasses = indicator.Indicator(GLASSES, 630, 360, ">>Cool Glasses<<", "glasses", 0)
-        self.sunglasses = indicator.Indicator(SUNGLASSES, 630, 360, ">>Sun Glasses<<", "glasses", 0)
-        self.blue_tshirt = indicator.Indicator(BLUE_TSHIRT, 630, 360, ">>blue t-shirt<<", "t-shirt", 0)
-        self.red_tshirt = indicator.Indicator(RED_TSHIRT, 630, 360, ">>red t-shirt<<", "t-shirt", 0)
-        self.yellow_tshirt = indicator.Indicator(YELLOW_TSHIRT, 630, 360, ">>yellow t-shirt<<", "t-shirt", 0)
-        self.boots = indicator.Indicator(BOOTS, 630, 360, ">>cool sneakers<<", "boots", 0)
-        self.items = [self.glasses, self.sunglasses, self.blue_tshirt, self.red_tshirt, self.yellow_tshirt, self.boots]
         # TEXT DATA -->
         self.font_50 = FONT_50
         self.font_100 = FONT_100
@@ -69,23 +63,36 @@ class Game:
         # OTHER -->
         self.satiety_gone = pg.USEREVENT + 1
         pg.time.set_timer(self.satiety_gone, 3000)
-        self.wear_num = []
         self.timer = 0
-        self.purchased_clothes = []
         # DataBase -->
-        self.player_db = database.Database("player.text", ["PlayerID", "satiety", "money", "happiness", "health"])
-        read_data = list(self.player_db.read("User1").values())
-        self.satiety_value = int(read_data[0])
-        self.money_value = int(read_data[1])
-        self.happiness_value = int(read_data[2])
-        self.health_value = int(read_data[3])
+        self.player_db = Database("player", ["PlayerID", "indicators", "clothes"])
+        read_data = self.player_db.read("User1")
+        self.satiety_value = read_data["indicators"]["satiety"]
+        self.money_value = read_data["indicators"]["money"]
+        self.happiness_value = read_data["indicators"]["happiness"]
+        self.health_value = read_data["indicators"]["health"]
+        # CLOTHES & ITEMS (DataBase) -->
+        self.purchased_clothes = read_data["clothes"]["purchased_clothes"]
+        self.worn = read_data["clothes"]["worn"]
+        self.glasses = Indicator(GLASSES, 630, 360, ">>Cool Glasses<<", "glasses",
+                                 self.worn[">>Cool Glasses<<"])
+        self.sunglasses = Indicator(SUNGLASSES, 630, 360, ">>Sun Glasses<<", "glasses",
+                                    self.worn[">>Sun Glasses<<"])
+        self.blue_tshirt = Indicator(BLUE_TSHIRT, 630, 360, ">>blue t-shirt<<", "t-shirt",
+                                     self.worn[">>blue t-shirt<<"])
+        self.red_tshirt = Indicator(RED_TSHIRT, 630, 360, ">>red t-shirt<<", "t-shirt",
+                                    self.worn[">>red t-shirt<<"])
+        self.yellow_tshirt = Indicator(YELLOW_TSHIRT, 630, 360, ">>yellow t-shirt<<", "t-shirt",
+                                       self.worn[">>yellow t-shirt<<"])
+        self.boots = Indicator(BOOTS, 630, 360, ">>cool sneakers<<", "boots", self.worn[">>cool sneakers<<"])
+        self.items = [self.glasses, self.sunglasses, self.blue_tshirt, self.red_tshirt, self.yellow_tshirt, self.boots]
 
     def events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                self.player_db.update(["User1", str(self.satiety_value), str(self.money_value),
-                                       str(self.happiness_value), str(self.health_value)])
-                self.player_db.save()
+                self.player_db.update(["User1", {"satiety": self.satiety_value, "money": self.money_value,
+                                                 "happiness": self.happiness_value, "health": self.health_value},
+                                       {"purchased_clothes": self.purchased_clothes, "worn": self.worn}])
                 pg.quit()
                 exit()
             elif event.type == pg.MOUSEBUTTONDOWN:
@@ -103,20 +110,22 @@ class Game:
                     if button_.rect.collidepoint(event.pos) and self.current_tub != 0:
                         # BUYING CLOTHES -->
                         if button_.text == "buy" and self.current_tub == 2:
-                            if self.option not in self.purchased_clothes:
+                            if not self.purchased_clothes[self.items[self.option].text]:
                                 if self.money_value >= self.clothing_prices[self.option]:
                                     self.money_value -= self.clothing_prices[self.option]
-                                    self.purchased_clothes.append(self.option)
+                                    self.purchased_clothes[self.items[self.option].text] = True
                         # PUTTING ON CLOTHES -->
                         elif button_.text == "wear" and self.current_tub == 2:
-                            if self.option not in self.wear_num and self.option in self.purchased_clothes:
-                                self.wear_num.append(self.option)
-                                self.items[self.option].worn = 1
-                                for wear in self.wear_num:
-                                    if self.items[wear] != self.items[self.option]:
-                                        if self.items[wear].type == self.items[self.option].type:
-                                            self.wear_num.remove(wear)
-                                            self.items[wear].worn = 0
+                            if (not self.items[self.option].worn
+                                    and self.purchased_clothes[self.items[self.option].text]):
+                                self.worn[self.items[self.option].text] = True
+                                self.items[self.option].worn = self.worn[self.items[self.option].text]
+                                for wear in self.items:
+                                    if wear.worn:
+                                        if wear != self.items[self.option]:
+                                            if wear.type == self.items[self.option].type:
+                                                self.worn[wear.text] = False
+                                                wear.worn = self.worn[wear.text]
                         # BUYING FOOD -->
                         elif button_.text == "eat" and self.current_tub == 1:
                             if self.money_value >= self.food_prices[self.option]:
@@ -144,7 +153,7 @@ class Game:
                     self.current_tub = 0
                     self.score_value = 0
                     self.toys = []
-                    self.pet_game.rect_col.x = 650
+                    self.pet_mini.rect_col.x = 650
                     self.option = 0
                     self.timer = 0
             # MINI GAME EVENTS -->
@@ -154,13 +163,13 @@ class Game:
                     if self.timer <= 4:
                         self.timer += 1
                     else:
-                        new_toy = mini_game.FallingToys(random.randint(145, 1100), 50)
+                        new_toy = FallingToys(random.randint(145, 1100), 50)
                         self.toys.append(new_toy)
                 if self.timer in [4, 5]:
-                    if key[pygame.K_a] is True and self.pet_game.rect.left > 145:
-                        self.pet_game.rect.x -= 15
-                    elif key[pygame.K_d] is True and self.pet_game.rect.right < 1515:
-                        self.pet_game.rect.x += 15
+                    if key[pygame.K_a] is True and self.pet_mini.rect.left > 145:
+                        self.pet_mini.rect.x -= 15
+                    elif key[pygame.K_d] is True and self.pet_mini.rect.right < 1515:
+                        self.pet_mini.rect.x += 15
 
             # DECREASED SATIETY -->
             if event.type == self.satiety_gone:
@@ -170,7 +179,7 @@ class Game:
         if self.current_tub == 3:
             for toy in self.toys:
                 toy.movement()
-                if toy.rect.colliderect(self.pet_game.rect_col):
+                if toy.rect.colliderect(self.pet_mini.rect_col):
                     self.score_value += 1
                     self.toys.remove(toy)
                 elif toy.rect.y > 750:
@@ -241,13 +250,13 @@ class Game:
                 self.items[self.option].render(self.display)
                 self.display.blit(BACK, RECT_BACK)
                 self.display.blit(FORWARD, RECT_FORWARD)
-                if self.option in self.wear_num:
+                if self.items[self.option].worn:
                     self.worn_top_label_on.render(self.display)
                     self.font_50.render_to(self.display, [1140, 250], self.worn_top_label_on.text, BROWN)
                 else:
                     self.worn_top_label_off.render(self.display)
                     self.font_50.render_to(self.display, [1088, 250], self.worn_top_label_off.text, BROWN)
-                if self.option in self.purchased_clothes:
+                if self.purchased_clothes[self.items[self.option].text]:
                     self.bought_top_label_on.render(self.display)
                     self.font_50.render_to(self.display, [1115, 150], self.bought_top_label_on.text, BROWN)
                 else:
@@ -257,7 +266,7 @@ class Game:
             elif self.current_tub == 3:
                 self.display.blit(GAME_BACKGROUND, (0, 0))
 
-                self.pet_game.render(self.display)
+                self.pet_mini.render(self.display)
                 if self.timer <= 3:
                     self.font_100.render_to(self.display, [675, 328], f"{str(self.timer)}...", BROWN)
                 if self.timer == 4:
